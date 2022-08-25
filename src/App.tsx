7b8e86e02e9ea511a96d78ae57beb8088d3f8817
lbuimistriuk <lbuimistriuk@neoflex.ru>
2022-08-25 14:07:28 +0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Switcher from "./components/Switcher/Switcher";
+import "./styles/App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render = () => {
+    return (
+        <div className="App">
+            <Switcher
+                options={[{value: "Hello"}, {value: "Modern"}, {value: "World"}]}
+            />
+        </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/Switcher/Switcher.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import '../../styles/Switcher.css';
+import _ from "lodash";
+
+type OptionType = {value: string, label?: string, disabled?: boolean};
+type SwitcherSizeType = "Small" | "Default" | "Large";
+type FontSizeType = "xs" | "sm" | "md";
+
+interface SwitcherProps {
+    options: OptionType[]
+    value?: string
+    onChange?: Function
+    disabled?: boolean
+    size?: SwitcherSizeType
+    fontSize?: FontSizeType
+}
+
+interface SwitcherState {
+    selectedValue?: string
+}
+
+class Switcher extends React.Component<SwitcherProps, SwitcherState> {
+    id: string
+    rootClassName: string
+
+    constructor(props: SwitcherProps) {
+        super(props);
+        this.state = {
+            selectedValue: props.value
+        }
+        this.id = _.uniqueId("switcher")
+        this.rootClassName = (
+            "switcher" +
+            " switcher--size-" + (props.size || "Default").toLowerCase() +
+            " switcher--font-" + (props.fontSize || "sm")
+        );
+    }
+
+    componentDidUpdate(prevProps: SwitcherProps, prevState: SwitcherState): void {
+        if (prevProps.value !== this.props.value) {
+            this.setState({selectedValue: this.props.value});
+        }
+    }
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedValue = e.target.value;
+        this.setState({selectedValue})
+        this.props.onChange && this.props.onChange(selectedValue)
+    }
+
+    render() {
+        return (
+            <div className={this.rootClassName}>
+            {
+                this.props.options.map((o:OptionType, i: number) => {
+                    const inputId: string = this.id + i;
+                    return (
+                        <div className={"switcher-item"}>
+                            <input
+                                type="radio"
+                                id={inputId}
+                                name={this.id}
+                                value={o.value}
+                                disabled={o.disabled || this.props.disabled}
+                                checked={o.value === (this.state.selectedValue)}
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor={inputId}>{o.label || o.value}</label>
+                        </div>
+                    );
+                })
+            }
+            </div>
+        )
+    }
+}
+
+export default Switcher
